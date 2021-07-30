b06898d0135c88b22cec9c137b6e6761db140619
mosesnjoroge19 <njorogemosesngure@gmail.com>
2021-07-30 04:55:31 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/greenscore/clima_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.greenscore.clima_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/scratch.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() => performTasks();
+
+void performTasks() {
+  task1();
+  task2();
+  task3();
+}
+
+void task1() {
+  String result = 'task1 data';
+  print('Task 1 complete');
+}
+
+void task2() {
+  String result = 'task2 data';
+  print('Task 1 complete');
+}
+
+void task3() {
+  String result = 'task3 data';
+  print('Task 1 complete');
+}
