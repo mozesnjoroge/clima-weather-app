84cd17933f0633be09fa2870e330b7ddf1b014aa
mosesnjoroge19 <njorogemosesngure@gmail.com>
2021-07-30 05:18:28 +0300

    Included scratch.dart for async testing

--- lib/scratch.dart
+++ lib/scratch.dart
@@ -14,11 +14,13 @@ void task1() {
 }
 
 void task2() {
+  Duration threeSeconds = Duration(seconds: 3);
+  sleep(threeSeconds);
   String result = 'task2 data';
-  print('Task 1 complete');
+  print('Task 2 complete');
 }
 
 void task3() {
   String result = 'task3 data';
-  print('Task 1 complete');
+  print('Task 3 complete');
 }
